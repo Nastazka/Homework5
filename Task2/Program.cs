ab3b15aef7fc41f5b6c8ef32122faf4b47358636
Anastasia <nastazka8@gmail.com>
2023-12-22 17:36:00 +0300

    добавила второе задание

---
+++ Task2/Program.cs
@@ -0,0 +1,50 @@
+﻿// // Задача 2: Задайте двумерный массив. Напишите программу, которая
+// // поменяет местами первую и последнюю строку массива.
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[,] array = new int[4, 4];
+
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                array[i, j] = new Random().Next(-10, 10);
+            }
+        }
+        Console.WriteLine("The random array:");
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                Console.Write("{0,3}", array[i, j]);
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine("Array after replasing strings:");
+        SwapRows(array);
+
+    }
+    
+    static void SwapRows(int[,] array)
+    {
+        for(int i = 0; i < array.GetLength(1); i++)
+            {
+                var tmp = array[3, i];
+                array[3, i] = array[0, i];
+                array[0, i] = tmp;
+            }
+            for(int i = 0; i < array.GetLength(0); i++)
+            {
+                for(int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write("{0,3}", array[i, j]);
+                }
+                Console.WriteLine();
+            }
+    }
+}
